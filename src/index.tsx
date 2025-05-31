d94e456bf53f06744f1ce3b2bdea60fc08008fc0
osk0830 <oshiro.sk.0830@gmail.com>
2025-05-31 22:12:11 +0900

    ビルドテスト

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import { createRoot } from 'react-dom/client'
+
+const Main = (<h1>Markdown Editor</h1>)
+
+const root = createRoot(document.getElementById('app'))
+root.render(Main)
